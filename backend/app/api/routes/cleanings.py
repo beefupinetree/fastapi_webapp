37c04967e60df14e4ff89582de53e1c0c8d9f190
Tarek Samhan <tsamhan@gmail.com>
2024-12-26 16:38:12 -0500

    Added all CRUD methods

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -1,7 +1,7 @@
 from typing import List
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException, Path
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
-from app.models.cleaning import CleaningCreate, CleaningPublic
+from app.models.cleaning import CleaningCreate, CleaningUpdate, CleaningPublic
 from app.db.repositories.cleanings import CleaningsRepository
 from app.api.dependencies.database import get_repository
 
@@ -48,3 +48,39 @@ async def get_cleaning_by_id(
         )
 
     return cleaning
+
+
+@router.put(
+    "/{id}/", response_model=CleaningPublic, name="cleanings:update-cleaning-by-id"
+)
+async def update_cleaning_by_id(
+    id: int = Path(..., ge=1, title="The ID of the cleaning to update."),
+    cleaning_update: CleaningUpdate = Body(..., embed=True),
+    cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> CleaningPublic:
+    updated_cleaning = await cleanings_repo.update_cleaning(
+        id=id,
+        cleaning_update=cleaning_update,
+    )
+
+    if not updated_cleaning:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail="No cleaning found with that id."
+        )
+    return updated_cleaning
+
+
+@router.delete("/{id}/", response_model=int, name="cleanings:delete-cleaning-by-id")
+async def delete_cleaning_by_id(
+    id: int = Path(..., ge=1, title="The ID of the cleaning to delete."),
+    cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> int:
+    deleted_id = await cleanings_repo.delete_cleaning_by_id(id=id)
+
+    if not deleted_id:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail="No cleaning found with that id.",
+        )
+
+    return deleted_id
